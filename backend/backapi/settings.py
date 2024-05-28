146c33f2068f155f8ac32a9196ed6f2521348fdd
Angel Moran <122565686+Angelmrn@users.noreply.github.com>
2024-05-27 21:52:00 -0600

    a

--- backend/backapi/settings.py
+++ backend/backapi/settings.py
@@ -82,17 +82,17 @@ WSGI_APPLICATION = 'backapi.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        #'HOST': 'localhost',
-        'HOST': 'bv6vhldetlmjgcpjbblh-mysql.services.clever-cloud.com',
+       'HOST': 'localhost',
+         #'HOST': 'bv6vhldetlmjgcpjbblh-mysql.services.clever-cloud.com',
         'PORT': '3306',
-        #'USER': 'root',
-        'USER': 'ulfresv6hzozbfwl',
-        #'PASSWORD': 'AngeldanMYSQL.2003',
+        'USER': 'root',
+        #'USER': 'ulfresv6hzozbfwl',
+        'PASSWORD': 'AngeldanMYSQL.2003',
         #'PASSWORD': 'Moran.12',
         #'PASSWORD': 'GAELROOT12',
-        'PASSWORD': 'i1JoM3UJadBNr2e3FgHf',
-        #'NAME': 'electric-hub',
-        'NAME': 'bv6vhldetlmjgcpjbblh',
+        #'PASSWORD': 'i1JoM3UJadBNr2e3FgHf',
+        'NAME': 'electric-hub',
+        #'NAME': 'bv6vhldetlmjgcpjbblh',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         }
